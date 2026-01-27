5fd2a11d35584e447e9e06821e0195ecb72f7413
FloritFlatsDemografia <igmoca1993@gmail.com>
2026-01-27 13:19:22 +0100

    Update pipeline.py

--- core/pipeline.py
+++ core/pipeline.py
@@ -1,13 +1,13 @@
-import streamlit as st
-from core.io_readers import read_file
-from core.cleaning import clean_cols
-from core.schema import detect_columns
-from core.dataset import build_dataset
+import pandas as pd
 
-@st.cache_data(show_spinner=True)
-def load_and_prepare(f_lista, f_listado):
-    df_lista = clean_cols(read_file(f_lista))
-    df_listado = clean_cols(read_file(f_listado))
-    cm = detect_columns(df_lista, df_listado)
-    data = build_dataset(df_lista, df_listado, cm)
-    return df_lista, df_listado, cm, data
+from core.cleaning import clean_df
+from core.io_readers import read_any
+
+
+def load_and_prepare(file) -> pd.DataFrame:
+    """
+    Carga archivo (CSV / XLS / XLSX) y aplica limpieza estándar
+    """
+    df = read_any(file)
+    df = clean_df(df)
+    return df
