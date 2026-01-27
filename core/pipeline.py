abbf331a83f1996de48b63aadc2b38dab7ef6f10
FloritFlatsDemografia <igmoca1993@gmail.com>
2026-01-27 13:24:57 +0100

    Update pipeline.py

--- core/pipeline.py
+++ core/pipeline.py
@@ -1,13 +1,9 @@
 import pandas as pd
-
-from core.cleaning import clean_df
 from core.io_readers import read_file
+from core.cleaning import clean_df
 
 
 def load_and_prepare(file) -> pd.DataFrame:
-    """
-    Carga archivo (CSV / XLS / XLSX) y aplica limpieza estándar
-    """
     df = read_file(file)
     df = clean_df(df)
     return df
