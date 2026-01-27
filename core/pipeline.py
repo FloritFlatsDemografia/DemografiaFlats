1c5a1163d45e12bfa08f51b5bbfa5e40640d62e6
FloritFlatsDemografia <igmoca1993@gmail.com>
2026-01-27 13:39:43 +0100

    Update pipeline.py

--- core/pipeline.py
+++ core/pipeline.py
@@ -1,13 +1,7 @@
-from core.cleaning import clean_listado_reservas
 from core.io_readers import read_any
+from core.cleaning import clean_listado_reservas
 
 
 def load_and_prepare(f_lista, f_listado):
-    """
-    Carga y limpia los archivos de reservas.
-    """
-
-    # Solo usamos LISTADO (el que tiene fechas, ingresos, etc.)
     df = clean_listado_reservas(read_any(f_listado))
-
     return df
