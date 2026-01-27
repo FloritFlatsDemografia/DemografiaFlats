07c3f1514b15d73a01a544d06773271a2ad89f39
FloritFlatsDemografia <igmoca1993@gmail.com>
2026-01-27 13:35:52 +0100

    Update pipeline.py

--- core/pipeline.py
+++ core/pipeline.py
@@ -1,16 +1,13 @@
+from core.cleaning import clean_listado_reservas
 from core.io_readers import read_any
-from core.cleaning import clean_lista_reservas, clean_listado_reservas
-import pandas as pd
 
 
-def load_and_prepare(f_lista, f_listado) -> pd.DataFrame:
-    df_lista = clean_lista_reservas(read_any(f_lista))
-    df_listado = clean_listado_reservas(read_any(f_listado))
+def load_and_prepare(f_lista, f_listado):
+    """
+    Carga y limpia los archivos de reservas.
+    """
 
-    df = df_lista.merge(
-        df_listado,
-        on="Localizador",
-        how="left"
-    )
+    # Solo usamos LISTADO (el que tiene fechas, ingresos, etc.)
+    df = clean_listado_reservas(read_any(f_listado))
 
     return df
